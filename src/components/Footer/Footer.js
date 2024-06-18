421b2d8baee2eab5ac00cff18e362cd2138e32cf
sneha kumari <snehakumari64445@gmail.com>
2024-06-18 18:30:49 +0530

    footer

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -33,10 +33,10 @@ const Footer = () => {
       <div className="footer-bottom">
         <p>&copy; 2024 Daisy Deals. All rights reserved.</p>
         <div className="social-icons">
-          <a href="#"><i className="fab fa-facebook-f"></i></a>
-          <a href="#"><i className="fab fa-instagram"></i></a>
-          <a href="#"><i className="fab fa-pinterest-p"></i></a>
-          <a href="#"><i className="fab fa-twitter"></i></a>
+          <a href="#"><i className="fab fa-facebook-f fi"></i></a>
+          <a href="#"><i className="fab fa-instagram ii"></i></a>
+          <a href="#"><i className="fab fa-pinterest-p pi"></i></a>
+          <a href="#"><i className="fab fa-twitter ti"></i></a>
         </div>
       </div>
     </footer>
